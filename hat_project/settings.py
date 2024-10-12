e420c94ccc60a2b2507fa0e92158e022f4c39039
Mhando-Z <Mhandosz17@gmail.com>
2024-10-13 02:34:10 +0300

    Phase Two|

--- hat_project/settings.py
+++ hat_project/settings.py
@@ -176,16 +176,16 @@ WSGI_APPLICATION = 'hat_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'hattz',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Mhando',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hattz',
+        'USER': 'postgres',
+        'PASSWORD': 'Mhando',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 DATABASES['default'] = dj_database_url.parse(config("DATABASE_URL"))
 
